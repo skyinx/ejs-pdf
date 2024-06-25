4a651060d14afd6ffbb70bef84ab1aac3210301b
Yash Kasetiya <sky.inx@protonmail.com>
2024-06-25 23:33:46 +0530

    Fix: CORS Issue.

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 
 const port = process.env.PORT || 3000; // Use environment variable for port or default to 3000
 
